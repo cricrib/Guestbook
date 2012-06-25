0f508c5710745dffec2855c0f225ccba5f4dd298
cricrib <cricrib@ymail.com>
2012-06-25 21:38:00 +0100

    Appengine tuto -- Datastore integration

--- src/guestbook/SignGuestbookServlet.java
+++ src/guestbook/SignGuestbookServlet.java
@@ -1,18 +1,25 @@
 package guestbook;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class SignGuestbookServlet extends HttpServlet {
+	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(SignGuestbookServlet.class
 					.getName());
 
@@ -22,15 +29,23 @@ public class SignGuestbookServlet extends HttpServlet {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 
+		// We have one entity group per Guestbook with all Greetings residing
+		// in the same entity group as the Guestbook to which they belong.
+		// This lets us run an ancestor query to retrieve all Greetings for a
+		// given Guestbook. However, the write rate to each Guestbook should be
+		// limited to ~1/second.
+		String guestbookName = req.getParameter("guestbookName");
+		Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
 		String content = req.getParameter("content");
-		if (content == null) {
-			content = "(No greeting)";
-		}
-		if (user != null) {
-			log.info("Greeting posted by user " + user.getNickname() + ": " + content);
-		} else {
-			log.info("Greeting posted anonymously: " + content);
-		}
-		resp.sendRedirect("/guestbook.jsp");
+		Date date = new Date();
+		Entity greeting = new Entity("Greeting", guestbookKey);
+		greeting.setProperty("user", user);
+		greeting.setProperty("date", date);
+		greeting.setProperty("content", content);
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.put(greeting);
+
+		resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
 	}
 }
